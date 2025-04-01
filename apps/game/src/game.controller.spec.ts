233fac200dd867502918a2fb9f59e9c385037542
praveen <praveen@taabsolutions.com>
2025-04-01 11:08:27 +0530

    first commit

---
+++ apps/game/src/game.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
+
+describe('GameController', () => {
+  let gameController: GameController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [GameController],
+      providers: [GameService],
+    }).compile();
+
+    gameController = app.get<GameController>(GameController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(gameController.getHello()).toBe('Hello World!');
+    });
+  });
+});
